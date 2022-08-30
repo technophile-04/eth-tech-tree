950ab550084790ee27d7d1adb3462cf9480ba63a
Carlos Sánchez <oceanrdn@gmail.com>
2022-08-30 10:05:15 +0200

    Basic structure + README

---
+++ README.md
@@ -0,0 +1,7 @@
+# ETH Tech Tree
+
+Set up:
+
+1. Clone repo
+2. `yarn install`
+3. `yarn dev`
---
+++ src/components/Tree.jsx
@@ -0,0 +1,9 @@
+const Tree = () => {
+  return (
+    <div className="tree">
+      <p>Tree</p>
+    </div>
+  )
+}
+
+export default Tree
